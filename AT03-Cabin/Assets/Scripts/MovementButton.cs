a2c847d21d34ba6fda0492de84f875b666705328
FaerieDust30 <160442076+FaerieDust30@users.noreply.github.com>
2024-04-24 12:22:08 +0800

    Final amendments
    Added if statement to the movement code so you can only move around with the number keys if the movement menu is enabled.
    Added clearer instructions for how to interact with the waypoints and the tooltips.
    Edited the arrow sprite so it's clearer to see.

--- AT03-Cabin/Assets/Scripts/MovementButton.cs
+++ AT03-Cabin/Assets/Scripts/MovementButton.cs
@@ -4,41 +4,50 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
-// Faye Butler - 22/04/2024
+// Faye Butler - 24/04/2024
 
 public class MovementButton : MonoBehaviour
 {
 
     [SerializeField] private Transform[] waypoint;
 
+    private LocationMovementMenu moveMenu;
+
+    private void Awake()
+    {
+        moveMenu = FindObjectOfType<LocationMovementMenu>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (moveMenu.menu.activeSelf == true)
         {
-            MoveToWaypoint(0);
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                MoveToWaypoint(0);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                MoveToWaypoint(1);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                MoveToWaypoint(2);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                MoveToWaypoint(3);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                MoveToWaypoint(4);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                MoveToWaypoint(5);
+            }
         }
-      if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            MoveToWaypoint(1);
-        }
-      if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            MoveToWaypoint(2);
-        }
-      if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            MoveToWaypoint(3);
-        }
-      if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            MoveToWaypoint(4);
-        }
-      if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            MoveToWaypoint(5);
-        }
-
     }
 
     public void MoveToWaypoint(int place)
